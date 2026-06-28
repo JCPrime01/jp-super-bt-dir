9173c20f455dfb911399b3fd606bd12c60510a29
JCPrime01 <vanzokiller@gmail.com>
2026-06-28 12:32:28 -0300

    chore: shareCode 35GSE5AH279 -> H71FQRMFJU6 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=35GSE5AH279&afp1=boost&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=H71FQRMFJU6&afp1=boost&home=1' });
     res.end();
   }
