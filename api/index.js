4b5f517ec5723b5794314c8e751a88f08a5eab7e
JCPrime01 <vanzokiller@gmail.com>
2026-07-03 21:46:33 -0300

    chore: shareCode 7X2JDXT282Q -> F290U89J3J8 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=7X2JDXT282Q&afp1=boost&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=F290U89J3J8&afp1=boost&home=1' });
     res.end();
   }
