481ecfbac898198cfaab9f17695002da284cd891
JCPrime01 <vanzokiller@gmail.com>
2026-07-08 17:33:17 -0300

    chore: shareCode HLY2NU8PFPZ -> SE6UU1QMQXS (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=HLY2NU8PFPZ&afp1=boost&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=SE6UU1QMQXS&afp1=boost&home=1' });
     res.end();
   }
