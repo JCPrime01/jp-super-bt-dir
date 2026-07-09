e712ae173dd734c5a18dc793d47a86d121e48c14
JCPrime01 <vanzokiller@gmail.com>
2026-07-09 08:52:48 -0300

    chore: shareCode SE6UU1QMQXS -> 6YJZVYFFUBT (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=SE6UU1QMQXS&afp1=boost&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=6YJZVYFFUBT&afp1=boost&home=1' });
     res.end();
   }
