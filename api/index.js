ad7ebdc4e55e7c1bd46471600fb1117caaf0e4e4
JCPrime01 <vanzokiller@gmail.com>
2026-07-11 11:21:22 -0300

    chore: shareCode VZ6229LQ57N -> UJD06JZZZJX (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=VZ6229LQ57N&afp1=boost&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=UJD06JZZZJX&afp1=boost&home=1' });
     res.end();
   }
