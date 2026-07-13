f4bee2959ef58811ddc0530931b115445c133c04
JCPrime01 <vanzokiller@gmail.com>
2026-07-13 14:39:51 -0300

    chore: shareCode BDRKZP80NC2 -> PHNTQGY2HHB (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=BDRKZP80NC2&afp1=boost&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=PHNTQGY2HHB&afp1=boost&home=1' });
     res.end();
   }
