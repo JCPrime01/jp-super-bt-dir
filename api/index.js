d105788217185e4f476d1cd328f58781bd497b33
JCPrime01 <vanzokiller@gmail.com>
2026-07-15 12:29:53 -0300

    chore: shareCode PHNTQGY2HHB -> E981U8JLVX3 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=PHNTQGY2HHB&afp1=boost&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=E981U8JLVX3&afp1=boost&home=1' });
     res.end();
   }
