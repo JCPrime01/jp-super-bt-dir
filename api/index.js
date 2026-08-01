b208fcb359cfb5061e076f8499129ce962ba791c
JCPrime01 <vanzokiller@gmail.com>
2026-08-01 11:29:59 -0300

    chore: shareCode PWTPBYRX8V8 -> OYJJ74X2FFM (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=PWTPBYRX8V8&afp1=boost&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=OYJJ74X2FFM&afp1=boost&home=1' });
     res.end();
   }
