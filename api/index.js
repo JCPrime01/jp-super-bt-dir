34e985daaf30522a5e87732926dcf39c359a7f99
JCPrime01 <vanzokiller@gmail.com>
2026-08-04 10:00:52 -0300

    chore: shareCode XWE9WO1UESM -> PJU17HMAHXE (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=XWE9WO1UESM&afp1=boost&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=PJU17HMAHXE&afp1=boost&home=1' });
     res.end();
   }
