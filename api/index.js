0e8713137ebf6a3f667ce24e1ef66057f90d22bf
JCPrime01 <vanzokiller@gmail.com>
2026-08-05 16:51:15 -0300

    chore: shareCode 53HAPPY1LQ9 -> VIDU5L8D7Y8 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=53HAPPY1LQ9&afp1=boost&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=VIDU5L8D7Y8&afp1=boost&home=1' });
     res.end();
   }
