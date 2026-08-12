3a5ad5c25a1388d63a9475a88397e53513a37999
JCPrime01 <vanzokiller@gmail.com>
2026-08-12 09:28:00 -0300

    chore: shareCode XAF95MAUPJS -> PA5GGEBYOUE (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=XAF95MAUPJS&afp1=boost&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=PA5GGEBYOUE&afp1=boost&home=1' });
     res.end();
   }
